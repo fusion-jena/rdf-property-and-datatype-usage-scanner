ff1540da6c23f4385880091a559089179f7de583
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2021-11-03 20:37:53 +0100

    fix warning

--- src/main/java/de/uni_jena/cs/fusion/experiment/rdf_datatype_usage/exceptions/NoItemException.java
+++ src/main/java/de/uni_jena/cs/fusion/experiment/rdf_datatype_usage/exceptions/NoItemException.java
@@ -1,6 +1,7 @@
 package de.uni_jena.cs.fusion.experiment.rdf_datatype_usage.exceptions;
 
 public class NoItemException extends Exception {
+	private static final long serialVersionUID = -6904029135170200860L;
 
 	public NoItemException(String message) {
 		super(message);
